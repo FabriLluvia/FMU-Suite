6911fe4e63a233ce7a86d2383e698f449a27aeeb
FabriLluvia <114926151+FabriLluvia@users.noreply.github.com>
2024-05-31 08:27:06 -0500

    Update TokenGenerator.py

--- TokenGenerator.py
+++ TokenGenerator.py
@@ -1,2 +1,17 @@
 import secrets
-secrets.token_bytes(16)
+
+TokenLength = int(input('Enter the number of bits of the token (Recomender 32 or 64): '))
+
+SecretToken = secrets.token_bytes(TokenLength)
+HexSecretToken = secrets.token_hex(TokenLength)
+UrlSecretToken = secrets.token_urlsafe(TokenLength)
+print("Token in bytes: ")
+print(SecretToken)
+print("====================")
+print(" ")
+print("Token in hex: ")
+print(HexSecretToken)
+print("====================")
+print(" ")
+print("Token in URL secret: ")
+print(UrlSecretToken)
